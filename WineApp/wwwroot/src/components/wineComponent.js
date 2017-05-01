a9981754d2bbda982f0302190d05ca4a3397e1fe
Anders Kofoed <anders.kfd@gmail.com>
2017-05-01 11:45:39 +0200

    Change display of count

--- WineApp/wwwroot/src/components/wineComponent.js
+++ WineApp/wwwroot/src/components/wineComponent.js
@@ -2,30 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const WineComponent = ({ wine }) => {
+  const info = wine.info;
   return (
-    <div className="card" key={wine.info.rowKey}>
+    <div className="card" key={info.rowKey}>
       <div className="card-block">
-        <h3 className="card-title">{wine.info.name} </h3>
-        <p className="card-text">Count: {wine.ids.length}</p>
+        <h3 className="card-title">{info.name} {wine.ids.length === 1 ? '' : `(${wine.ids.length})`} </h3>
         <button
           className="btn btn-info"
           type="button"
           data-toggle="collapse"
-          data-target={`#infoSection-${wine.info.rowKey}`}
+          data-target={`#infoSection-${info.rowKey}`}
           aria-expanded="false"
-          aria-controls={`infoSection-${wine.info.rowKey}`}
+          aria-controls={`infoSection-${info.rowKey}`}
         >
           Info
         </button>
 
-        <div className="collapse" id={`infoSection-${wine.info.rowKey}`}>
+        <div className="collapse" id={`infoSection-${info.rowKey}`}>
           <div className="card card-block">
             <ul>
-              <li>Producer: {wine.info.producer}</li>
-              <li>vintage: {wine.info.vintage}</li>
-              <li>Area: {wine.info.area}</li>
-              <li>Fruit: {wine.info.fruit}</li>
-              <li>Fruit: {wine.info.boughtDate}</li>
+              <li>Producer: {info.producer}</li>
+              <li>vintage: {info.vintage}</li>
+              <li>Area: {info.area}</li>
+              <li>Fruit: {info.fruit}</li>
+              <li>Fruit: {info.boughtDate}</li>
             </ul>
           </div>
         </div>
