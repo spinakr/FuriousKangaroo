c976d968a0216f63323291518611031a675cab04
Anders Kofoed <anders.kfd@gmail.com>
2017-05-01 21:21:00 +0200

    Refactor wineInfo to component

--- WineApp/wwwroot/src/components/wineComponent.js
+++ WineApp/wwwroot/src/components/wineComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import WineInfoComponent from './wineInfoComponent';
 
 const WineComponent = ({ wine }) => {
   const info = wine.info;
@@ -14,39 +15,29 @@ const WineComponent = ({ wine }) => {
         aria-controls={`infoSection-${info.rowKey}`} key={info.rowKey}
       >
         <h5 className="card-title">{info.name} {wine.ids.length === 1 ? '' : `(${wine.ids.length})`} </h5>
-
       </div>
 
       <div className="collapse" id={`infoSection-${info.rowKey}`}>
         <div className="container">
+
           <div className="row">
             <div className="col-sm-6">
-              <div className="card-block">
-                <h6 className="card-subtitle">Producer</h6>
-                <p className="card-text">{info.producer}</p>
-              </div>
+              <WineInfoComponent label="Producer" data={info.producer} />
             </div>
             <div className="col-sm-6">
-              <div className="card-block">
-                <h6 className="card-subtitle">Vintage</h6>
-                <p className="card-text">{info.vintage}</p>
-              </div>
+              <WineInfoComponent label="Vintage" data={info.vintage} />
             </div>
           </div>
+
           <div className="row">
             <div className="col-sm-6">
-              <div className="card-block">
-                <h6 className="card-subtitle">Fruit</h6>
-                <p className="card-text">{info.fruit}</p>
-              </div>
+              <WineInfoComponent label="Fruit" data={info.fruit} />
             </div>
             <div className="col-sm-6">
-              <div className="card-block">
-                <h6 className="card-subtitle">Area</h6>
-                <p className="card-text">{info.area}, {info.country}</p>
-              </div>
+              <WineInfoComponent label="Area" data={`${info.area}, ${info.country}`} />
             </div>
           </div>
+
         </div>
       </div>
     </div>
