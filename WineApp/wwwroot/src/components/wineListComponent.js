84eb781cf9811b6a7b0624b5087ae61ec7dc6a35
Anders Kofoed <anders.kfd@gmail.com>
2017-05-01 11:30:33 +0200

    Order cards

--- WineApp/wwwroot/src/components/wineListComponent.js
+++ WineApp/wwwroot/src/components/wineListComponent.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import GridComponent from './gridComponent';
 import Winecomponent from './wineComponent';
 
 const WineListComponent = ({ wines }) => {
   return (
-    <GridComponent columns="3">
+    <div className="card-columns">
       {wines.map((wine, index) => {
         return <Winecomponent wine={wine} key={index} />;
       })}
-    </GridComponent>
+    </div>
   );
 };
 
